d3c41c7753f2305c07cbcacdc1478b7927ba9db9
vatsalAgrawal0 <vatsalagrawal148@gmail.com>
2025-06-07 03:59:19 +0530

    Initializing Repo

---
+++ src/main/java/com/trading/TradingAlgoApplication.java
@@ -0,0 +1,13 @@
+package com.trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingAlgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingAlgoApplication.class, args);
+	}
+
+}
